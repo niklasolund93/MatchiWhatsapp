4498c38f021b398b810f2c6054c1c1bbb955a7d6
niklas ölund 93 <niklasolund@hotmail.com>
2026-03-18 10:53:57 +0100

    första version
    lägger upp en första version

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# MatchiWhatsapp
-script som kollar om det finns tider lediga och skickar ut dessa till önskat whatsappgrupp
+# Matchi Tennis/Padel/Sqush/Badminton webscraper och push till polare
+
+Automatiskt script som:
+- söker lediga tennistider (eller padel, går att ändra i script) via Matchi
+- söker de klubbar som är intressanta för dig att leta i, ändra detta i script. använd samma stavning som matchi!
+- filtrerar kvällstider, detta går att ändra till de tider som passar dig/er. Filtrerar även så den endast letar på mån-tors, detta kan du själv justera i scriptet såklart
+- letar endast 7 dagar framåt då de klubbar jag har endast släpper tider för 7 dagar framåt, detta kan du också ändra..
+- skickar notis via WhatsApp Web
+
+---
+
+## Funktioner
+
+- "Scraping" av Matchi API
+- WhatsApp automation via Selenium
+- Test mode (skickar inget, sparar inget)
+- behåller chrome session (slipp QR-login varje gång, och kan ha det på i bakgrunden)
+- schemaläggning efter önskemål
+
+## Information kring setup
+För att köra så behöver du installera de bibliotek som behövs manuellt eller via requirements.txt:
+1. requests & requirements för matchi scraping
+2. selenium för automatiserat whatsapputskick
+3. schedule för schemaläggning
+4. Första gången du kör scriptet så behöver du ha mobilen redo att logga in med QR koden
+5. efter du loggat in kan du köra scriptet igen och denna gången så kommer den (förhoppningsvis) lyckas hitta chatten och skriva meddelandet
+6. när du är nöjd med vad den hittar och meddelandet så kan du ändra till "TEST_MODE = False"
+7. nu kan den vara igång i bakgrunden av din dator så körs den automatiskt. OBS! har problem med headerless (chrome är osynlig och körs som dold process) och behålla sessionen så för nu så måste den poppa upp för datorn/servern som kör scriptet. 
+
+
+## Saker att justera i scriptet!!
+
+1. TEST_MODE - true/false
+2. FACILITIES - alla som du vill söka igenom efter tider, exakt namn från match
+3. GROUP_NAME - exakta namnet på den whatsapp grupp du vill skriva till
+4. "sport" - ändra till den siffran som du vill söka efter, ex 5 = padel, 1 = tennis, 2 = badminton, 3 = squash
+5. current.weekday() - här jag har exkluderat 4(fredag),5(lördag),6(söndag)
+6. valid - här har jag 18-20 då jag endast vill spela efter jobbet men inte för sent
+7. msg - text som kommer innan tidförslagen
+8. schedule - schemaläggning, jag väljer att köra denna varje dag kl 07
+
+---
+
+## Installation
+
+```bash
+git clone https://github.com/your-repo/matchi-bot.git
+cd matchi-bot
+pip install -r requirements.txt
---
+++ checkTimes.py
@@ -0,0 +1,212 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import schedule
+import time
+import json
+import re
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+URL = "https://www.matchi.se/book/findFacilities"
+
+
+#ändra detta till den/de klubbar du vill söka tider hos
+FACILITIES = [
+    "Spårvägens Tennisklubb",
+    "Mälarhöjdens IK Tennis"
+]
+
+#detta är exakta namnet på den whatsappp grupp du vill skicka meddelande till
+GROUP_NAME = "test autoutskick"
+
+#detta är filen som den sparar ned tidigare utskick till, detta för att den inte skall spamma samma tid flera dagar i chatten
+SAVED_FILE = "sent_slots.json"
+
+#ändra till True för att inte spara historik och inte skicka meddelande i chatten
+#ändra till False för att köra på riktigt
+TEST_MODE = True
+
+HEADERS = {
+    "accept": "*/*",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "origin": "https://www.matchi.se",
+    "referer": "https://www.matchi.se/book/index",
+    "user-agent": "Mozilla/5.0",
+    "x-requested-with": "XMLHttpRequest"
+}
+
+
+def load_sent():
+    if TEST_MODE:
+        return set()  
+    try:
+        with open(SAVED_FILE, "r", encoding="utf-8") as f:
+            return set(json.load(f))
+    except:
+        return set()
+
+def save_sent(data):
+    if TEST_MODE:
+        return  #  spara inte i testläge
+    with open(SAVED_FILE, "w", encoding="utf-8") as f:
+        json.dump(list(data), f)
+
+
+# det som skickat i POST body till matchi
+def get_slots(date, facility):
+
+    payload = {
+        "lat": "",
+        "lng": "",
+        "offset": "0",
+        "outdoors": "",
+        "sport": "1",       #5 = padel, 1 = tennis, 2 = badminton, 3 = squash
+        "date": date.strftime("%Y-%m-%d"),
+        "q": facility,
+        "hasCamera": ""
+    }
+
+    r = requests.post(URL, headers=HEADERS, data=payload)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    slots = []
+
+    for btn in soup.select("button.btn-slot"):
+        text = btn.get_text(" ", strip=True)
+        m = re.search(r"\b(\d{1,2})\b", text)
+        if m:
+            slots.append(int(m.group(1)))
+    return slots
+
+
+def find_new_slots():
+
+    sent = load_sent()
+    new = []
+    today = datetime.today()
+    end_date = today + timedelta(days=7)
+    current = today
+
+    while current <= end_date:
+
+        if current.weekday() not in [4, 5, 6]:
+            for facility in FACILITIES:
+                slots = get_slots(current, facility)
+                valid = [h for h in slots if 18 <= h <= 20]
+
+                for hour in valid:
+                    slot_id = f"{facility}-{current.date()}-{hour}"
+                    if slot_id not in sent:
+                        new.append((facility, current.date(), hour))
+                        sent.add(slot_id)
+
+                time.sleep(1)
+        current += timedelta(days=1)
+
+    save_sent(sent)
+    return new
+
+
+
+def build_message(slots):
+
+    msg = "hej, det finns tider tillgängliga vill någon spela tennis på följande?\n\n"
+    for f, d, h in slots:
+        msg += f"{f} {d} {h}:00\neller\n"
+
+    msg = msg.rstrip("eller\n")
+    return msg
+
+
+
+def send_whatsapp(message):
+
+    options = webdriver.ChromeOptions()
+    #skapar sökväg till chrome profilen i samma mapp som PY filen ligger, profil krävs för att spara inlogg till whatsapp!
+    BASE_DIR = Path(__file__).resolve().parent
+    PROFILE_DIR = BASE_DIR / "chrome_profile"
+    options.add_argument(f"--user-data-dir={PROFILE_DIR}") 
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://web.whatsapp.com")
+
+    wait = WebDriverWait(driver, 30)
+
+    print("Logga in i WhatsApp Web...")
+    time.sleep(20)
+
+    search = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "//div[@contenteditable='true'][@data-tab='3']")
+    ))
+
+    search.click()
+    search.clear()
+    search.send_keys(GROUP_NAME)
+
+    chat = wait.until(EC.presence_of_element_located(
+        (By.XPATH, f"//span[@title='{GROUP_NAME}']")
+    ))
+    chat.click()
+
+    box = wait.until(EC.presence_of_all_elements_located(
+        (By.XPATH, "//div[@contenteditable='true']")
+    ))[-1]
+
+    for line in message.split("\n"):
+        box.send_keys(line)
+        box.send_keys(Keys.SHIFT, Keys.ENTER)
+
+    if TEST_MODE:
+        print("\n====================")
+        print("TEST MODE - Klickar inte Enter/send")
+        time.sleep(10)
+        driver.quit()
+        print("Stoppar scriptet..")
+        time.sleep(2)
+        exit()
+        return
+
+    box.send_keys(Keys.ENTER)
+
+    print("WhatsApp message sent")
+
+    time.sleep(3)
+    driver.quit()
+
+
+def job():
+
+    print("Checking slots", datetime.now())
+    slots = find_new_slots()
+
+    if not slots:
+        print("No new slots")
+        return
+
+    message = build_message(slots)
+    print(message)
+    send_whatsapp(message)
+
+
+if __name__ == "__main__":
+
+    job()
+
+    if not TEST_MODE:
+        schedule.every().day.at("07:00").do(job)
+
+        print("Scheduler running...")
+
+        while True:
+            schedule.run_pending()
+            time.sleep(30)
